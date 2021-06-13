c07c13d8e9b7204326821740ead645ef7c2fbfba
jim3692 <jim3692@gmail.com>
2021-06-13 04:24:12 +0300

    changes

---
+++ script.js
@@ -0,0 +1,46 @@
+;(() => {
+  // Set background based on day
+  document.body.style.background = 'url(resources/backgrounds/' + new Date().getDay() + '.jpg)'
+  document.body.style.backgroundColor = 'black'
+  document.body.style.backgroundRepeat = 'no-repeat'
+  document.body.style.backgroundAttachment = 'fixed'
+
+  const until = document.getElementById('until')
+
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    document.body.style.backgroundSize = 'auto'
+    document.body.style.backgroundPosition = '50% 0%'
+    until.style.maxWidth = '600px'
+  } else {
+    document.body.style.backgroundSize = 'cover'
+    document.body.style.backgroundPosition = '50% 50%'
+  }
+
+  // Set the date we're counting down to
+  const countDownDate = new Date('Jan 21, 2022 00:00:00').getTime()
+
+  // Update the count down every 1 second
+  const x = setInterval((function loop () {
+    // Get today's date and time
+    const now = new Date().getTime()
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    if (distance < 0) {
+      clearInterval(x)
+      until.innerText = 'it is finally time!'
+    } else {
+      // Display the result in the element with id="until"
+      until.innerText = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`
+    }
+
+    return loop
+  })(), 1000)
+})()
