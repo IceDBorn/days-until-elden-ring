68684cd785beaf39835c6993f53ec0f5737394c9
jim3692 <jim3692@gmail.com>
2021-06-13 07:00:22 +0300

    Add sparks

---
+++ sparks.js
@@ -0,0 +1,101 @@
+;(() => {
+  window.requestAnimFrame = (function () { return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (a) { window.setTimeout(a, 1E3 / 60) } }())
+
+  const c = document.getElementById('sparks')
+  const ctx = c.getContext('2d')
+  const cw = c.width = window.innerWidth
+  const ch = c.height = window.innerHeight
+  const rand = function (a, b) { return ~~((Math.random() * (b - a + 1)) + a) }
+  const plusses = []
+  const count = 300
+  let tick = 10
+  const tickMax = 10
+
+  const Plus = function () {
+    this.init()
+  }
+
+  Plus.prototype.init = function () {
+    this.x = cw * Math.random()
+    this.y = ch * 1
+    this.vx = (rand(0, 100) - 50) / 12
+    this.vy = -(rand(50, 100)) / 90
+    this.lightness = Math.random() < 0.7 ? rand(45, 50) : rand(65, 70)
+    this.alpha = 1
+    this.fade = 1
+    this.scale = 0.01
+    this.growth = 0.01
+    this.rotation = 8
+    this.spin = (rand(0, 100) - 50) / 3000
+  }
+
+  Plus.prototype.update = function (i) {
+    this.x += this.vx
+    this.y -= this.vy
+    this.vy += 0.15 * this.scale
+    if (this.alpha < 1) {
+      this.alpha += this.fade
+    }
+
+    this.scale += this.growth
+    this.rotation += this.spin
+
+    if (this.y - 30 >= ch) {
+      this.init()
+    }
+
+    if (this.y < 0) {
+      return false
+    }
+
+    return true
+  }
+
+  Plus.prototype.render = function () {
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.scale(this.scale, this.scale)
+    ctx.rotate('80')
+
+    ctx.fillStyle = 'hsla(20, 200%, ' + this.lightness + '%, 1)'
+    ctx.beginPath()
+    ctx.rect(-3, -1, 6, 2)
+    ctx.fill()
+    ctx.restore()
+  }
+
+  const createPlusses = function () {
+    if (plusses.length < count) {
+      if (tick >= tickMax) {
+        plusses.push(new Plus())
+        tick = 0
+      } else {
+        tick++
+      }
+    }
+  }
+
+  const updatePlusses = function () {
+    let i = plusses.length
+    while (i--) {
+      plusses[i].update(i) || plusses.splice(i, 1)
+    }
+  }
+
+  const renderPlusses = function () {
+    let i = plusses.length
+    while (i--) {
+      plusses[i].render()
+    }
+  }
+
+  const loop = function () {
+    window.requestAnimFrame(loop)
+    ctx.clearRect(0, 0, cw, ch)
+    createPlusses()
+    updatePlusses()
+    renderPlusses()
+  }
+
+  loop()
+})()
