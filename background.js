bb891d79ed4b15fcb5599eeaa4ecb57755f06f42
IceDBorn <fundwork@tutanota.com>
2021-06-16 04:10:12 +0300

    Add new screenshots - new functions to select a random image per day based on the image name

--- background.js
+++ background.js
@@ -1,20 +1,35 @@
 ;(() => {
   const isMobile = /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-
+  const rand = function (a, b) { return ~~((Math.random() * (b - a + 1)) + a) }
   let today
 
-  setBackground()
+  getBackground()
 
   setInterval((function loop () {
     if (today !== new Date().getDay()) {
-      setBackground()
+      getBackground()
     }
     return loop
   })(), 1000)
 
-  function setBackground () {
+  function getBackground () {
     today = new Date().getDay()
-    document.body.style.background = 'black url(resources/backgrounds/' + new Date().getDay() + '.jpg)'
+    backgroundExists('resources/backgrounds/' + new Date().getDay() + '-' + rand(0, 1) + '.jpg')
+  }
+
+  function backgroundExists (url) {
+    window.fetch(url, { method: 'HEAD' })
+      .then(res => {
+        if (res.ok) {
+          SetBackground(url)
+        } else {
+          SetBackground('resources/backgrounds/' + (new Date().getDay()) + '-0.jpg')
+        }
+      })
+  }
+
+  function SetBackground (url) {
+    document.body.style.background = 'black url(' + url + ')'
     document.body.style.backgroundRepeat = 'no-repeat'
 
     if (isMobile) {
